05f74ed7d4d8cb60f74a1ae3ecbfabdb26b05220
hussariusz <burneracc@tuta.com>
2026-03-17 18:53:40 +0100

    deploy

--- src/components/LatestEvents.tsx
+++ src/components/LatestEvents.tsx
@@ -3,12 +3,18 @@ import EventNewsTeaser from './EventNewsTeaser';
 import styles from '@/app/ui/LatestEvents.module.css';
 
 const LatestEvents = async () => {
-  const { data } = await fetchData();  
+  const { data } = await fetchData();
+  const { data: pinnedData } = await fetchPinnedData();
+  console.log(pinnedData); // Debugging informati
   return (
-    <section className={styles.latest}>      
-      <ul className={styles.list}> {data.stories?.map((nestedBlok: any) => (
-        <li key={nestedBlok.uuid}><EventNewsTeaser blok={nestedBlok} /></li>
-      ))}</ul>
+    <section className={styles.latest}>
+      <ul className={styles.list}>
+        {pinnedData.stories?.map((nestedBlok: any) => (
+          <li key={nestedBlok.uuid}><EventNewsTeaser blok={nestedBlok} /></li>
+        ))}
+        {data.stories?.map((nestedBlok: any) => (
+          <li key={nestedBlok.uuid}><EventNewsTeaser blok={nestedBlok} /></li>
+        ))}</ul>
     </section>);
 };
 
@@ -20,9 +26,26 @@ export async function fetchData() {
     version: 'published',
     starts_with: 'event/',
     filter_query: {
-      component: { in: 'event-news' }
+      component: { in: 'event-news' },
     },
     sort_by: 'content._meta.first_published_at:desc',
     per_page: 3
   })
+}
+
+export async function fetchPinnedData() {
+  const storyblokApi = getStoryblokApi();
+  const response = await storyblokApi.get(`cdn/stories/`, {
+    version: 'published',
+    starts_with: 'event/',
+    filter_query: {
+      and: [
+        { tags: { in_array: 'angeheftet' } },
+        { component: { in: 'event-news' } }
+      ]
+    },
+    sort_by: 'content._meta.first_published_at:desc',
+  })
+  const filteredStories = response.data.stories.filter((story: any) => story.tag_list && story.tag_list.length > 0);
+  return { ...response, data: { ...response.data, stories: filteredStories } };
 }
