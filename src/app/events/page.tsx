66ad641dfd34b48887e7b66a9e60385d875d37bd
hussariusz <burneracc@tuta.com>
2026-03-19 21:19:51 +0100

    header for news

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -4,6 +4,7 @@ import { getStoryblokApi } from '@/lib/storyblok';
 import styles from '@/app/page.module.css';
 import { useEffect, useState } from 'react';
 import Pagination from '@/components/Pagination';
+import HeroImage from '@/components/HeroImage';
 
 type TotalPages = {
   totalPages?: number
@@ -18,34 +19,36 @@ interface DataResult {
 export default function Home() {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageData, setPageData] = useState<DataResult & TotalPages>();
+  const [pageHeaderData, setPageHeaderData] = useState();
 
   useEffect(() => {
     const fetchDataInHook = async () => {
       try {
         const { data, perPage, total } = await fetchData(currentPage);
-        setPageData({ data, perPage, total, totalPages: Math.ceil(total / perPage) });        
+        const { data: headerData } = await fetchHeaderData();
+
+        setPageHeaderData(headerData);
+        setPageData({ data, perPage, total, totalPages: Math.ceil(total / perPage) });
       } catch (error) {
         console.log(error);
       }
     }
     fetchDataInHook();
   }, [currentPage]);
-  
+
   return (
     <div className={styles.page}>
       <main>
-        <header>
-          <h1>Aktuelles</h1>
-        </header>
-        <ul className='list'>
-          {pageData?.data?.stories.map((story: any) => (
-            <li key={story.uuid}>
-              <EventNewsTeaser blok={story} />
-            </li>
-          ))}
-        </ul>
-        <Pagination nextPageHandler={() => setCurrentPage(currentPage + 1)} pageChangeHandler={(page:number)=>setCurrentPage(page)} prevPageHandler={() => setCurrentPage(currentPage - 1)} currentPage={currentPage} totalPages={pageData?.totalPages} />
-      </main>
+      {pageHeaderData && <HeroImage blok={pageHeaderData?.story.content.body[0]} />}
+      <ul className='list'>
+        {pageData?.data?.stories.map((story: any) => (
+          <li key={story.uuid}>
+            <EventNewsTeaser blok={story} />
+          </li>
+        ))}
+      </ul>
+      <Pagination nextPageHandler={() => setCurrentPage(currentPage + 1)} pageChangeHandler={(page: number) => setCurrentPage(page)} prevPageHandler={() => setCurrentPage(currentPage - 1)} currentPage={currentPage} totalPages={pageData?.totalPages} />
+</main>
     </div>
   );
 }
@@ -59,3 +62,11 @@ export async function fetchData(page = 1, per_page = 8): Promise<DataResult> {
     per_page
   });
 }
+
+export async function fetchHeaderData() {
+  const storyblokApi = getStoryblokApi();
+  return await storyblokApi.get(`cdn/stories/events`, {
+    version: 'published',
+  });
+}
+
